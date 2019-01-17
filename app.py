aad3334254cf8ca6628324af3d4141e73e5a1fc2
Dhanush Dazz <37190602+Dhanush4444@users.noreply.github.com>
2019-01-17 16:05:38 +0530

    Updated with GPIO

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# import RPi.GPIO as gp
+import RPi.GPIO as gp
 import pyrebase
 config = {
     'apiKey': "AIzaSyBJIreESnah74oUzVMy10VcUW-pX_4xiKM",
@@ -10,65 +10,84 @@ config = {
   }
 firebase = pyrebase.initialize_app(config)
 database=firebase.database()
-
-# print(database.child("items").child('1').child('name').get().val())
-
-prevValue= database.child("items").child('1').child('prev').get().val()
+coke =3
+kit =5
+sup =7
+lays =11
+dairy = 13
 # print(prevValue)
-
+gp.setmode(gp.BOARD)
+gp.setup(coke,gp.OUT)
+gp.setup(kit,gp.OUT)
+gp.setup(sup,gp.OUT)
+gp.setup(lays,gp.OUT)
+gp.setup(dairy,gp.OUT)
 def cokeData():
-  prevValue= database.child("items").child('1').child('prev').get().val()
-  presentValue=database.child("items").child('1').child('count').get().val()
-  return prevValue-presentValue,presentValue
+	prevValue= database.child("items").child('1').child('prev').get().val()
+	presentValue=database.child("items").child('1').child('count').get().val()
+	return prevValue-presentValue,presentValue
 
 def kitData():
-  prevValue= database.child("items").child('2').child('prev').get().val()
-  presentValue=database.child("items").child('2').child('count').get().val()
-  return prevValue-presentValue,presentValue
+	prevValue= database.child("items").child('2').child('prev').get().val()
+	presentValue=database.child("items").child('2').child('count').get().val()
+	return prevValue-presentValue,presentValue
+
 
 def supData():
-  prevValue= database.child("items").child('3').child('prev').get().val()
-  presentValue=database.child("items").child('3').child('count').get().val()
-  return prevValue-presentValue,presentValue
+	prevValue= database.child("items").child('3').child('prev').get().val()
+	presentValue=database.child("items").child('3').child('count').get().val()
+	return prevValue-presentValue,presentValue
 
 def laysData():
-  prevValue= database.child("items").child('4').child('prev').get().val()
-  presentValue=database.child("items").child('4').child('count').get().val()
-  return prevValue-presentValue,presentValue
+	prevValue= database.child("items").child('4').child('prev').get().val()
+	presentValue=database.child("items").child('4').child('count').get().val()
+	return prevValue-presentValue,presentValue
 
 def dairyData():
-  prevValue= database.child("items").child('5').child('prev').get().val()
-  presentValue=database.child("items").child('5').child('count').get().val()
-  return prevValue-presentValue,presentValue
+	prevValue= database.child("items").child('5').child('prev').get().val()
+	presentValue=database.child("items").child('5').child('count').get().val()
+	return prevValue-presentValue,presentValue
 
 
 while True:
-  selected=database.child('selected').get().val()
-  if selected == 1:
-    a,b=cokeData()
-    if a !=0:
-      print('Data Changed')
-      database.child('items').child('1').child('prev').set(b)
-  elif selected == 2:
-    a,b=kitData()
-    if a !=0:
-      print('Data Changed')
-      database.child('items').child('2').child('prev').set(b)
-  elif selected == 3:
-    a,b=supData()
-    if a !=0:
-      print('Data Changed')
-      database.child('items').child('3').child('prev').set(b)
-  elif selected == 4:
-    a,b=laysData()
-    if a !=0:
-      print('Data Changed')
-      database.child('items').child('4').child('prev').set(b)
-  elif selected == 5:
-    a,b=dairyData()
-    if a !=0:
-      print('Data Changed')
-      database.child('items').child('5').child('prev').set(b)
-    
-
-  
+	selected=database.child('selected').get().val()
+	if selected == 1:
+		a,b=cokeData()
+		if a !=0:
+			#print('Data Changed')
+			database.child('items').child('1').child('prev').set(b)
+			gp.output(coke,1)
+		else:
+			gp.output(coke,0)
+	elif selected == 2:
+		a,b=kitData()
+		if a !=0:
+			#print('Data Changed')
+			database.child('items').child('2').child('prev').set(b)
+			gp.output(kit,1)
+		else:
+			gp.output(kit,0)
+	elif selected == 3:
+		a,b=supData()
+		if a !=0:
+			#print('Data Changed')
+			database.child('items').child('3').child('prev').set(b)
+			gp.output(sup,1)
+		else:
+			gp.output(sup,0)
+	elif selected == 4:
+		a,b=laysData()
+		if a !=0:
+			#print('Data Changed')
+			database.child('items').child('4').child('prev').set(b)
+			gp.output(lays,1)
+		else:
+			gp.output(lays,0)
+	elif selected == 5:
+		a,b=dairyData()
+		if a !=0:
+			#print('Data Changed')
+			database.child('items').child('5').child('prev').set(b)
+			gp.output(dairy,1)
+		else:
+gp.output(dairy,0)
