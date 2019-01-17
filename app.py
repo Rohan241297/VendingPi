5f4432a8820fb6bbcfa2e765eafbf58245aeb035
Dhanush4444 <dhanushdazz@gmail.com>
2019-01-17 15:25:13 +0530

    Added selected dataChange

--- app.py
+++ app.py
@@ -16,10 +16,59 @@ database=firebase.database()
 prevValue= database.child("items").child('1').child('prev').get().val()
 # print(prevValue)
 
-while True:
+def cokeData():
   prevValue= database.child("items").child('1').child('prev').get().val()
   presentValue=database.child("items").child('1').child('count').get().val()
-  if prevValue-presentValue != 0:
-    print('DataChanged')
-    database.child("items").child("1").child("prev").set(presentValue)
+  return prevValue-presentValue,presentValue
+
+def kitData():
+  prevValue= database.child("items").child('2').child('prev').get().val()
+  presentValue=database.child("items").child('2').child('count').get().val()
+  return prevValue-presentValue,presentValue
+
+def supData():
+  prevValue= database.child("items").child('3').child('prev').get().val()
+  presentValue=database.child("items").child('3').child('count').get().val()
+  return prevValue-presentValue,presentValue
+
+def laysData():
+  prevValue= database.child("items").child('4').child('prev').get().val()
+  presentValue=database.child("items").child('4').child('count').get().val()
+  return prevValue-presentValue,presentValue
+
+def dairyData():
+  prevValue= database.child("items").child('5').child('prev').get().val()
+  presentValue=database.child("items").child('5').child('count').get().val()
+  return prevValue-presentValue,presentValue
+
+
+while True:
+  selected=database.child('selected').get().val()
+  if selected == 1:
+    a,b=cokeData()
+    if a !=0:
+      print('Data Changed')
+      database.child('items').child('1').child('prev').set(b)
+  elif selected == 2:
+    a,b=kitData()
+    if a !=0:
+      print('Data Changed')
+      database.child('items').child('2').child('prev').set(b)
+  elif selected == 3:
+    a,b=supData()
+    if a !=0:
+      print('Data Changed')
+      database.child('items').child('3').child('prev').set(b)
+  elif selected == 4:
+    a,b=laysData()
+    if a !=0:
+      print('Data Changed')
+      database.child('items').child('4').child('prev').set(b)
+  elif selected == 5:
+    a,b=dairyData()
+    if a !=0:
+      print('Data Changed')
+      database.child('items').child('5').child('prev').set(b)
+    
+
   
